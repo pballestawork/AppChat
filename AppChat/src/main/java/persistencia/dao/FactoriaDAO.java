48fd0ea5706c19056243d848d81510f6053c8341
Pablo Ballesta Rodriguez <pballestawork@gmail.com>
2025-01-19 23:24:17 +0100

    FactoriaDAO stub

---
+++ AppChat/src/main/java/persistencia/dao/FactoriaDAO.java
@@ -0,0 +1,46 @@
+package persistencia.dao;
+
+public abstract class FactoriaDAO {
+	private static FactoriaDAO unicaInstancia;
+	
+	//Ubicación de la clase la factoria
+	public static final String DAO_TDS = "persistencia.dao.TDSFactoriaDAO";
+
+	/**
+	 * Constructor vacío
+	 */
+	protected FactoriaDAO() {}
+	
+	/**
+	 * 
+	 * @param tipo Nombre de la clase (incluido el paquete) Ej: 'persistencia.dao.FactoriaDAO'
+	 * @return Factoria única
+	 * @throws DAOException
+	 */
+	@SuppressWarnings("deprecation")
+	public static FactoriaDAO getInstancia(String tipo) throws DAOException{
+		if (unicaInstancia == null) {
+			try {
+				unicaInstancia= (FactoriaDAO) Class.forName(tipo).newInstance();
+			}catch (Exception e) {
+				throw new DAOException(e.getMessage());
+			}
+		}
+		return unicaInstancia;
+	}
+	
+	
+	
+	public static FactoriaDAO getInstancia() throws DAOException {
+		if(unicaInstancia == null)
+			return getInstancia(DAO_TDS);//TODO
+		else
+			return unicaInstancia;
+	}
+	
+	public abstract IAdaptadorUsuarioDAO getUsuarioDAO();
+	public abstract IAdaptadorGrupoDAO getGrupoDAO();
+	public abstract IAdaptadorContactoIndividualDAO getContactoIndividualDAO();
+	public abstract IAdaptadorMensajeDAO getMensajeDAO();
+	
+}
---
+++ AppChat/src/main/java/persistencia/dao/TDSFactoriaDAO.java
@@ -0,0 +1,31 @@
+package persistencia.dao;
+
+public class TDSFactoriaDAO extends FactoriaDAO {
+
+	public TDSFactoriaDAO() {}
+	
+	@Override
+	public IAdaptadorUsuarioDAO getUsuarioDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IAdaptadorGrupoDAO getGrupoDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IAdaptadorContactoIndividualDAO getContactoIndividualDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IAdaptadorMensajeDAO getMensajeDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
