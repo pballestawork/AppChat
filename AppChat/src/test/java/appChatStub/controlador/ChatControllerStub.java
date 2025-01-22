42a7e04b62caec52c10056b04a4bb20ba5c461ab
Pablo Ballesta Rodriguez <pballestawork@gmail.com>
2025-01-22 14:18:56 +0100

    LauncherStub v0.1 para pruebas de interfaz

---
+++ AppChat/src/test/java/appChatStub/controlador/ChatControllerStub.java
@@ -0,0 +1,51 @@
+package appChatStub.controlador;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import dominio.modelo.Contacto;
+import dominio.modelo.Grupo;
+import dominio.modelo.Mensaje;
+import dominio.modelo.Usuario;
+
+public class ChatControllerStub {
+	private Usuario usuario;
+	
+	public ChatControllerStub () {
+		usuario = new Usuario(0,"Pablo","600600600","pablo@gmail.com","1234","",false,null);
+		usuario.setContactos(new LinkedList<Contacto>());
+	}
+	
+	public Usuario iniciarSesion(String telefono, String contrasena) {
+		if(usuario.getTelefono() == telefono && usuario.getContrasena() == contrasena)
+			return usuario;
+		
+		return null;
+	}
+	
+	public void registrarUsuario(Usuario usuario) {
+		// Código para registrar un usuario
+	}
+	
+	public void enviarMensaje(Usuario emisor, Usuario receptor, String contenido) {
+		// Código para enviar un mensaje
+	}
+	
+	public Grupo crearGrupo(Usuario administrador,List<Usuario>  miembros ,String nombreGrupo) {
+		// Código para crear un grupo
+		return null;
+	}
+
+	public List<Mensaje> buscarMensajes(String filtro) {
+		// Código para buscar mensajes
+		return null;
+	}
+	
+	public void añadirContacto(String nombre, String numero) {
+		// Código para añadir un contacto
+	}
+	
+	public void exportarMensajesPDF(Usuario usuario,List<Mensaje> mensajes) {
+        // Código para exportar mensajes a PDF
+    }
+}
