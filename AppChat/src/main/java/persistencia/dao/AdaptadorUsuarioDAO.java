00ee803327ce113a3c665cc0a0f01e5e420c22ee
Pablo Ballesta Rodriguez <pballestawork@gmail.com>
2025-01-26 15:53:33 +0100

    Adaptador Usuario cambiado

--- AppChat/src/main/java/persistencia/dao/AdaptadorUsuarioDAO.java
+++ AppChat/src/main/java/persistencia/dao/AdaptadorUsuarioDAO.java
@@ -1,6 +1,5 @@
 package persistencia.dao;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,16 +16,19 @@ import utils.Utils;
 
 public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 	private static ServicioPersistencia servicioPersistencia;
-	private static PoolDAO poolDAO;
 	private static AdaptadorUsuarioDAO unicaInstancia = null;
-	private static final String PROP_NOMBRE = "nombre";
-	private static final String PROP_TELEFONO = "telefono";
-	private static final String PROP_EMAIL = "email";
-	private static final String PROP_CONTRASENA = "contrasena";
-	private static final String PROP_FOTO_PERFIL = "fotoPerfil";
-	private static final String PROP_ES_PREMIUM = "esPremium";
-	private static final String PROP_CONTACTOS = "contactos";
+	private PoolDAO poolDAO;
+	private IAdaptadorContactoIndividualDAO adaptadorContactoIndividual;
+	private IAdaptadorGrupoDAO adaptadorGrupo;
+	
 	public static final String TIPO_USUARIO = "Usuario";
+	private final String PROP_NOMBRE = "nombre";
+	private final String PROP_TELEFONO = "telefono";
+	private final String PROP_EMAIL = "email";
+	private final String PROP_CONTRASENA = "contrasena";
+	private final String PROP_FOTO_PERFIL = "fotoPerfil";
+	private final String PROP_ES_PREMIUM = "esPremium";
+	private final String PROP_CONTACTOS = "contactos";
 
 	/**
 	 * Constructor privado -> singleton
@@ -34,6 +36,12 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 	private AdaptadorUsuarioDAO() {
 		servicioPersistencia = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
 		poolDAO = PoolDAO.getUnicaInstancia();
+		try {
+			adaptadorContactoIndividual = FactoriaDAO.getInstancia(FactoriaDAO.DAO_TDS).getContactoIndividualDAO();
+			adaptadorGrupo = FactoriaDAO.getInstancia(FactoriaDAO.DAO_TDS).getGrupoDAO();
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -49,20 +57,19 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 	}
 
 	@Override
-	public int add(Usuario elemento) throws DAOException {
+	public void add(Usuario elemento) {
 		//1. Se comprueba que no esta registrada la entidad
 		Entidad nuevaEntidad = servicioPersistencia.recuperarEntidad(elemento.getId());
 		if (nuevaEntidad != null)
-			throw new DAOException("El usuario " + elemento.getNombre() + " ya existe");
+			return;
 
-		// TODO 2. registrar los contactos en ContactoDAO
-		String contactosId = registrarMasObtenerContactosId(elemento.getContactos());
-		
-		
+		// 2. Se registran sus objetos referenciados
+		registrarContactos(elemento.getContactos());
 		
 		// 3. Crea la entidad
 		nuevaEntidad = new Entidad();
 		nuevaEntidad.setNombre(TIPO_USUARIO);
+		
 		// 4. Añadir propiedades a la entidad
 		List<Propiedad> propiedadesLst = new LinkedList<Propiedad>();
 		propiedadesLst.add(new Propiedad(PROP_NOMBRE, elemento.getNombre()));
@@ -71,45 +78,83 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 		propiedadesLst.add(new Propiedad(PROP_CONTRASENA, elemento.getContrasena()));
 		propiedadesLst.add(new Propiedad(PROP_FOTO_PERFIL, elemento.getFotoPerfil()));
 		propiedadesLst.add(new Propiedad(PROP_ES_PREMIUM, elemento.isEsPremium() ? "true" : "false"));// FAQ reconoce el booleano?
-		propiedadesLst.add(new Propiedad(PROP_CONTACTOS, contactosId));
+		propiedadesLst.add(new Propiedad(PROP_CONTACTOS, Utils.concatenarIds(elemento.getContactos())));
 		nuevaEntidad.setPropiedades(propiedadesLst);
 
 		// 5. Se registra la entidad y se asocia su id con el objeto
 		nuevaEntidad = servicioPersistencia.registrarEntidad(nuevaEntidad);
 		elemento.setId(nuevaEntidad.getId());
-
-		// 6. Se añade al poolDAO (opcional)
-		poolDAO.addObjeto(elemento.getId(), elemento);// FAQ El poolDAO debería almacenar todos los usuarios?
-
-		return elemento.getId();
 	}
 
 	@Override
 	public void delete(Usuario elemento) {
-		// TODO Auto-generated method stub
+		Entidad entidad = servicioPersistencia.recuperarEntidad(elemento.getId());
 		
+		for (Contacto c: elemento.getContactos()) {
+			switch (entidad.getNombre()) {
+            case AdaptadorContactoIndividualDAO.TIPO_CONTACTO_INDIVIDUAL:
+                adaptadorContactoIndividual.delete((ContactoIndividual) c);
+                break;
+            case AdaptadorGrupoDAO.TIPO_GRUPO:
+                adaptadorGrupo.delete((Grupo) c);
+                break;
+            default:
+			}	
+		} 
+		
+		servicioPersistencia.borrarEntidad(entidad);
+		//TODO borrar del poolDAO
 	}
 
 	@Override
 	public void update(Usuario elemento) {
-		// TODO Auto-generated method stub
+		Entidad entidad = servicioPersistencia.recuperarEntidad(elemento.getId());
 		
+		for (Propiedad prop: entidad.getPropiedades()) {
+			switch (prop.getNombre()) {
+			case PROP_CONTACTOS:
+				prop.setValor(Utils.concatenarIds(elemento.getContactos()));
+				break;
+			case PROP_CONTRASENA:
+				prop.setValor(elemento.getContrasena());
+				break;
+			case PROP_EMAIL:
+				prop.setValor(elemento.getEmail());
+				break;
+			case PROP_ES_PREMIUM:
+				prop.setValor(elemento.isEsPremium() ? "true" : "false");
+				break;
+			case PROP_FOTO_PERFIL:
+				prop.setValor(elemento.getFotoPerfil());
+				break;
+			case PROP_NOMBRE:
+				prop.setValor(elemento.getNombre());
+				break;
+			case PROP_TELEFONO:
+				prop.setValor(elemento.getTelefono());
+				break;
+			default:
+				break;
+			}
+			servicioPersistencia.modificarPropiedad(prop);
+		}
 	}
 
 	@Override
-	public Usuario getById(int id) throws DAOException {
-
+	public Usuario getById(int id) {
+		// 1. Si está en el PoolDAO se retorna
 		if (poolDAO.contiene(id))
 			return (Usuario) poolDAO.getObjeto(id);
 
+		// 2. Recuperar la entidad, si no existe devolver null
 		Entidad e = servicioPersistencia.recuperarEntidad(id);
-
 		if (e == null)
-			throw new DAOException("El usuario con " + id + " no existe");
+			return null;
 
+		// 3. Inicializamos objeto con propiedades basicas y añadir al poolDAO
 		Usuario usuario = new Usuario();
 
-		// Recuperamos propiedades que no son objetos!!!
+			// 3.1 Recuperamos propiedades que no son objetos!!!
 		usuario.setId(e.getId());
 		usuario.setNombre(servicioPersistencia.recuperarPropiedadEntidad(e, PROP_NOMBRE));
 		usuario.setTelefono(servicioPersistencia.recuperarPropiedadEntidad(e, PROP_TELEFONO));
@@ -118,11 +163,14 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 		usuario.setFotoPerfil(servicioPersistencia.recuperarPropiedadEntidad(e, PROP_FOTO_PERFIL));
 		usuario.setEsPremium(servicioPersistencia.recuperarPropiedadEntidad(e, PROP_ES_PREMIUM) == "true");
 
+			// 3.2 Añadimos en el poolDAO para evitar bidireccionalidad infinita
 		poolDAO.addObjeto(e.getId(), usuario);
 		
+		// 4. Recuperar referenciados y actualizar objeto
 		List<Contacto> contactos = obtenerContactos(servicioPersistencia.recuperarPropiedadEntidad(e, PROP_CONTACTOS));
 		usuario.setContactos(contactos);
 		
+		// 5. Retornar objeto
 		return usuario;
 	}
 
@@ -130,12 +178,8 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 	public List<Usuario> getAll() {
 		List<Usuario> usuarios = new LinkedList<Usuario>();
 
-		for (Entidad entidad : servicioPersistencia.recuperarEntidades(TIPO_USUARIO)) {
-			try {
-				usuarios.add(getById(entidad.getId()));
-			} catch (DAOException e) {
-			} // No tiene sentido que no existan usuarios que acabamos de recuperar
-		}
+		for (Entidad entidad : servicioPersistencia.recuperarEntidades(TIPO_USUARIO))
+			usuarios.add(getById(entidad.getId()));
 
 		return usuarios;
 	}
@@ -145,23 +189,25 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 	 * @param contactos
 	 * @throws DAOException
 	 */
-	private String registrarMasObtenerContactosId(List<Contacto> contactos) throws DAOException {
-		IAdaptadorContactoIndividualDAO adapatadorContactoDAO = TDSFactoriaDAO.getInstancia().getContactoIndividualDAO();
-		IAdaptadorGrupoDAO adapatadorGrupoDAO = TDSFactoriaDAO.getInstancia().getGrupoDAO();
+	private void registrarContactos(List<Contacto> contactos) {
 		for (Contacto contacto: contactos) {
 			if (contacto instanceof ContactoIndividual) {
                 ContactoIndividual individual = (ContactoIndividual) contacto;
-				adapatadorContactoDAO.add(individual);
+				adaptadorContactoIndividual.add(individual);
             } else if (contacto instanceof Grupo) {
                 Grupo grupo = (Grupo) contacto;
-                adapatadorGrupoDAO.add(grupo);
+                adaptadorGrupo.add(grupo);
             }
 		}
-		
-		//Concatenamos los id
-		return Utils.concatenarIds(contactos);
 	}
 	
+	/*TODO recuperar los contactos (contactoIndividual+Grupo) utilizando una lista de Ids.
+	 * NOTA: utilizando Id no distingues entre tipos de contacto entonces no se puede
+	 * utilizar el contactoIndividualDAO o GrupoDAO. 
+	 * NOTA 2: Es importante utilizar el DAO para evitar recursividad infinita por
+	 * bidireccionalidad 
+	 * NOTA 3: Como solución temporal se hará directamente sobre el servicioPersitencia
+	 */
 	/**
 	 * Obtiene la lista de contactos a partir de una cadana parseada con los id de
 	 * cada contacto.
@@ -169,35 +215,31 @@ public class AdaptadorUsuarioDAO implements IAdaptadorUsuarioDAO {
 	 * @param recuperarPropiedadEntidad Ej "10,20,33,46"
 	 * @return lista de contactos asociados a los id
 	 * @throws DAOException 
-	 */
-	private List<Contacto> obtenerContactos(String contactosIds) throws DAOException {
-		List<Integer> ids = Utils.getIdsByCadena(contactosIds);
-		/*TODO recuperar los contactos (contactoIndividual+Grupo) utilizando una lista de Ids.
-		 * NOTA: utilizando Id no distingues entre tipos de contacto entonces no se puede
-		 * utilizar el contactoIndividualDAO o GrupoDAO. 
-		 * NOTA 2: Es importante utilizar el DAO para evitar recursividad infinita por
-		 * bidireccionalidad 
-		 * NOTA 3: Como solución temporal se hará directamente sobre el servicioPersitencia
-		 */
-		List<Entidad> entidadesContacto = new LinkedList<Entidad>();
-		List<Contacto> contactos = new LinkedList<Contacto>();
-		
-		ids.stream().forEach(id -> entidadesContacto.add(servicioPersistencia.recuperarEntidad(id)));
-		
-		IAdaptadorContactoIndividualDAO adaptadorContacto = TDSFactoriaDAO.getInstancia().getContactoIndividualDAO();
-		IAdaptadorGrupoDAO adaptadorGrupo= TDSFactoriaDAO.getInstancia().getGrupoDAO();
-		
-		for (Entidad entidad : entidadesContacto) {
-			if(entidad.getNombre()== AdaptadorContactoIndividualDAO.TIPO_CONTACTO_INDIVIDUAL) {
-				Contacto c = adaptadorContacto.getById(entidad.getId());
-				contactos.add(c);
-			}else if(entidad.getNombre()== AdaptadorGrupoDAO.TIPO_GRUPO) {
-				Contacto c = adaptadorGrupo.getById(entidad.getId());
-				contactos.add(c);
-			}
-		}
-		
-		return contactos;
+	 */	
+	private List<Contacto> obtenerContactos(String contactosIds) {
+	    // Lista de Ids
+	    List<Integer> ids = Utils.getIdsByCadena(contactosIds);
+
+	    // Recuperar las entidades
+	    List<Entidad> entidadesContacto = ids.stream()
+	            .map(id -> servicioPersistencia.recuperarEntidad(id))
+	            .collect(Collectors.toList());
+
+	    // Segun la entidad, generar la lista de contactos
+	    List<Contacto> contactos = new LinkedList<>();
+	    for (Entidad entidad : entidadesContacto) {
+	        switch (entidad.getNombre()) {
+	            case AdaptadorContactoIndividualDAO.TIPO_CONTACTO_INDIVIDUAL:
+	                contactos.add(adaptadorContactoIndividual.getById(entidad.getId()));
+	                break;
+	            case AdaptadorGrupoDAO.TIPO_GRUPO:
+	                contactos.add(adaptadorGrupo.getById(entidad.getId()));
+	                break;
+	            default:
+	        }
+	    }
+
+	    return contactos;
 	}
 
 }
--- AppChat/src/main/java/persistencia/dao/DAO.java
+++ AppChat/src/main/java/persistencia/dao/DAO.java
@@ -12,49 +12,49 @@ public interface DAO<T> {
 	/**
 	 * 1. Se comprueba que no esta registrada la entidad
 	 * 2. Se registran sus objetos referenciados
-	 * 3. Crea la entidad y asignamos id
+	 * 3. Crea la entidad
 	 * 4. Añadir propiedades a la entidad
 	 * 5. Se registra la entidad y se asocia su id con el objeto
-	 * 6. Se añade al poolDAO (opcional)
 	 * @param elemento
-	 * @return
-	 * @throws DAOException
 	 */
-	public int add(T elemento) throws DAOException;
+	public void add(T elemento);
 		
 	/**
 	 * 1. Recuperar entidad
 	 * 2. Eliminar agregados
 	 * 3. Eliminar entidad
 	 * @param elemento
-	 * @throws DAOException
 	 */
-	public void delete(T elemento) throws DAOException;
-	
+	public void delete(T elemento);
 	
 	/**
 	 * 1. Recuperar entidad
 	 * 2. Recorrer propiedades moficicando el valor
 	 * @param elemento
-	 * @throws DAOException
 	 */
-	public void update(T elemento) throws DAOException;	
-		
+	public void update(T elemento);	
+	
+	// 1. Si está en el PoolDAO se retorna
+	// 2. Recuperar la entidad, si no existe devolver null
+	// 3. Inicializamos objeto con propiedades basicas y añadir al poolDAO 
+		//3.1 Recuperamos propiedades que no son objetos!!!
+		//3.2 Añadimos en el poolDAO para evitar bidireccionalidad infinita
+	// 4. Recuperar referenciados y actualizar objeto
+	// 5. Retornar objeto	
 	/** 
 	 * 1. Si está en el PoolDAO se retorna
-	 * 2. Recuperar la entidad, si no existe lanzar excepción
-	 * 3. Inicializamos objeto con propiedades obtenidas y añadir al poolDAO 
+	 * 2. Recuperar la entidad, si no existe devolver null
+	 * 3. Inicializamos objeto con propiedades basicas y añadir al poolDAO 
 	 * 4. Recuperar referenciados y actualizar objeto
 	 * 5. Retornar objeto
 	 * @param id
 	 * @return
-	 * @throws DAOException
 	 */
-	public T getById(int id) throws DAOException;
+	public T getById(int id);
 	
 	/**
 	 * Devuelve todos los elementos del tipo indicado 
-	 * @throws DAOException 
+ 
 	 */
 	public List<T> getAll();
 }
